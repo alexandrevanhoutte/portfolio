7f345ede8b49a49f0ec5c9a2b166c19becc0a821
alexandrevanhoutte <alexandre.vanhoutte@gmail.com>
2024-04-12 23:05:58 +0900

    add about me section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header/Header";
 import "../app/globals.css";
+import AboutMe from "@/components/aboutMe/AboutMe";
 
 export default function Home() {
   return (
     <main>
-      <Header></Header>
+      <Header />
+      <AboutMe />
     </main>
   );
 }
