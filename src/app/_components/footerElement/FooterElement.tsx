b2844693c112aeb2c8c2fb169bb791e604f5b6b4
alexandrevanhoutte <alexandre.vanhoutte@gmail.com>
2024-04-13 11:18:30 +0900

    add footer logo

--- src/app/_components/footerElement/FooterElement.tsx
+++ src/app/_components/footerElement/FooterElement.tsx
@@ -1,14 +1,18 @@
 import styles from "./footerElement.module.css";
 
 type FooterElementProps = {
-  icon: string;
+  icon: JSX.Element;
   value: string;
+  link: string;
 };
 
 export default function FooterElement(props: FooterElementProps) {
   return (
     <div className={styles.footerElement}>
-      <span>{props.value}</span>
+      <a href={props.link} className={styles.link}>
+        <div className={styles.icon}>{props.icon}</div>
+        <div>{props.value}</div>
+      </a>
     </div>
   );
 }
--- src/app/_components/footerSection/FooterSection.tsx
+++ src/app/_components/footerSection/FooterSection.tsx
@@ -1,23 +1,32 @@
 import FooterElement from "@/app/_components/footerElement/FooterElement";
+import { LogoGithub } from "@/svg/LogoGithub";
+import { LogoLinkedin } from "@/svg/logoLinkedin";
 import styles from "./footerSection.module.css";
 
 interface FooterElement {
-  icon: string;
+  icon: JSX.Element;
   value: string;
+  link: string;
 }
 
 export default function FooterSection() {
   const footerElementList: FooterElement[] = [
     {
-      icon: "test",
-      value: "test",
+      icon: <LogoLinkedin />,
+      value: "Linkedin",
+      link: "https://www.linkedin.com/in/alexandre-vanhoutte/",
+    },
+    {
+      icon: <LogoGithub />,
+      value: "Github",
+      link: "https://github.com/alexandrevanhoutte/",
     },
   ];
 
   return (
     <div className={styles.footer}>
       {footerElementList.map((e, index) => (
-        <FooterElement key={index} icon={e.icon} value={e.value} />
+        <FooterElement key={index} {...e} />
       ))}
     </div>
   );
