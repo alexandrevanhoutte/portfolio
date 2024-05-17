9601789ce87a944ffd2063eab7f6c7b1d20fde95
alexandrevanhoutte <alexandre.vanhoutte@gmail.com>
2024-05-17 15:09:28 +0900

    update cors

--- src/app/api/contact/route.tsx
+++ src/app/api/contact/route.tsx
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-export const corsHeaders = {
+const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
