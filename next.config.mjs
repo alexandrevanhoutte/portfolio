8c8a6efc8adc4547aa7eb8e7966dbae2b59aa13b
alexandrevanhoutte <alexandre.vanhoutte@gmail.com>
2024-05-20 15:27:49 +0900

    update background

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['mdx', 'md', 'jsx', 'js', 'tsx'],
+  pageExtensions: ["mdx", "md", "jsx", "js", "tsx"],
 };
 
 export default nextConfig;
