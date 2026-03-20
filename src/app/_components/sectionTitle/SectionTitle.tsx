6640325f6637f8ef8a7ab06bb0796a059ae5d960
Alexandre Vanhoutte <alexandrevanhoutte@Alexandres-Mac-mini.local>
2026-03-20 23:40:06 +0900

    feat: update about me

--- src/app/_components/sectionTitle/SectionTitle.tsx
+++ src/app/_components/sectionTitle/SectionTitle.tsx
@@ -2,8 +2,13 @@ import styles from "./sectionTitle.module.css";
 
 type SectionTitleProps = {
   title: string;
+  className?: string;
 };
 
 export default function SectionTitle(props: SectionTitleProps) {
-  return <div className={styles.sectionTitle}>{props.title}</div>;
+  return (
+    <div className={`${styles.sectionTitle} ${props.className ?? ""}`}>
+      {props.title}
+    </div>
+  );
 }
